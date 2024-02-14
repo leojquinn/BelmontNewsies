8795eb4b028b7c1d2a5ef2b846c4625f028c6427
Leo Quinn <159583643+leojquinn@users.noreply.github.com>
2024-02-14 01:16:08 +0000

    Cheeseburger

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> p
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="Belmont.png" className="App-logo" alt="logo" />
         <p>
-          GitHub  <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          Belmont Newsies
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/IS8ZkRPA-pI?si=48cN6SLWEw4IhaOU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </p>
+        <p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/W4_nOTvd4nY?si=wcsKamE8T0whLl8S&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </p>
+        <p><iframe width="560" height="315" src="https://www.youtube.com/embed/epyLctpdGY0?si=KJ7unWeejc_JCMBs&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+      <p><iframe width="560" height="315" src="https://www.youtube.com/embed/J2P-vU5EHBY?si=i6MspbJIh1vgdnUA&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+      <p><iframe width="560" height="315" src="https://www.youtube.com/embed/tOtVkaGqtbQ?si=Q1KZnyJU7CAhg8ho&amp;start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
+      
+      
       </header>
     </div>
+    
+
   );
 }
 
